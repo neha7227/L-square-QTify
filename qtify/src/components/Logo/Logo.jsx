dd20222ee5f03ee9f62263599dd822686857ac92
neha7227 <neha.indoliya27@gmail.com>
2023-11-18 01:25:24 +0530

    fixed body scrolling issue

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
+import styles from "./Logo.module.css";
 function Logo() {
   return (
     <>
-      <img src={LogoImage} alt="logo" width={67} />
+      <img src={LogoImage} alt="logo" width={67} className={styles.logo} />
     </>
   );
 }
